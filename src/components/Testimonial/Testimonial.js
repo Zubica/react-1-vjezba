842e76509d1d6556c63ebb196513c9bea9b1718e
luka-mod <lmodric2311@gmail.com>
2023-03-17 17:54:28 +0100

    added testimonial

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import "./Testimonial.scss";
+import TestimonialImg from "../../assets/images/testimonial.jpg";
 
 const Testimonial = ({
-imgSrc, imgAlt, 
-content,
-children,
-isSecondary = false,
+  imgSrc,
+  imgAlt,
+  content,
+  children,
+  isSecondary = false,
 }) => {
   console.log(content);
 
   return (
-<section className={`Section $ {isSecondary  ? "Section_secondary" : ""}`}>
-        <div className ="Section-Inner">
-          <div className="Testimonial">
-            <figure className ="Testimonial-Figure">
-              <img
-                src={imgSrc}
-                alt={imgAlt}
-                className="Testimonial-Image"
-              />
-            </figure>
-            <p className="Testimonial-Content">
-              <span className="Testimonial-Quotation">"</span> At the academy, I
-              learned how to apply technology in practice. Frontend education
-              was demanding, it required a lot of time. Upon completion, I got
-              my first job as a developer.<span className="Testimonial-Guotation"
-                >"</span>
-            </p>
-          </div>
+    <section className={`Section ${isSecondary ? "Section_secondary" : ""}`}>
+      <div className="Section-Inner">
+        <div className="Testimonial">
+          <figure className="Testimonial-Figure">
+            <img
+              src={TestimonialImg}
+              alt={imgAlt}
+              className="Testimonial-Image"
+            />
+          </figure>
+          <p className="Testimonial-Content">
+            <span className="Testimonial-Quotation">"</span> At the academy, I
+            learned how to apply technology in practice. Frontend education was
+            demanding, it required a lot of time. Upon completion, I got my
+            first job as a developer.
+            <span className="Testimonial-Guotation">"</span>
+          </p>
         </div>
-      </section>
+      </div>
+    </section>
   );
 };
 
 Testimonial.propTypes = {
-  imgAlt:PropTypes.string,
-  imgSrc:PropTypes.string,
-  content:PropTypes.string,
-  isSecondary:PropTypes.bool,
-  children:PropTypes.node
-}
+  imgAlt: PropTypes.string,
+  imgSrc: PropTypes.string,
+  content: PropTypes.string,
+  isSecondary: PropTypes.bool,
+  children: PropTypes.node,
+};
 
-export default Testimonial
+export default Testimonial;
