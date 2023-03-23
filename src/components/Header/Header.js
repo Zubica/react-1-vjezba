9f778b642a4444598161b1210615556685f19470
luka-mod <lmodric2311@gmail.com>
2023-03-23 21:47:27 +0100

    add hard

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,19 @@
 import React from "react";
-import Button from "../Button/Button";
-import "./Header.scss";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import {
+  HeaderWrapper,
+  HeaderInner,
+  Hamburger,
+  Nav,
+  HeaderLink,
+  HeaderButton,
+} from "./HeaderStyles";
 
 const Header = ({ isSecondary = false }) => {
   return (
-    <header className={`Header ${isSecondary ? "Header_secondary" : ""}`}>
-      <div className="Header-Inner">
+    <HeaderWrapper isSecondary={isSecondary}>
+      <HeaderInner>
         <Link to={"/"}>
           <svg
             width="120"
@@ -22,32 +28,17 @@ const Header = ({ isSecondary = false }) => {
             />
           </svg>
         </Link>
-        <svg
-          className="Header-Hamburger"
-          width="448"
-          height="392"
-          viewBox="0 0 448 392"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M16 72H432C440.837 72 448 64.837 448 56V16C448 7.163 440.837 0 432 0H16C7.163 0 0 7.163 0 16V56C0 64.837 7.163 72 16 72ZM16 232H432C440.837 232 448 224.837 448 216V176C448 167.163 440.837 160 432 160H16C7.163 160 0 167.163 0 176V216C0 224.837 7.163 232 16 232ZM16 392H432C440.837 392 448 384.837 448 376V336C448 327.163 440.837 320 432 320H16C7.163 320 0 327.163 0 336V376C0 384.837 7.163 392 16 392Z"
-            fill="black"
-          />
-        </svg>
-        <nav className="Header-Nav">
-          <Link to={"/courses"} className="Header-Link">
-            Courses
-          </Link>
-          <Button className="Header-Button">Sign in</Button>
-          <Button className="Header-Button" isSecondary={true}>
-            Register
-          </Button>
-        </nav>
-      </div>
-    </header>
+        <Hamburger />
+        <Nav>
+          <HeaderLink to={"/courses"}>Courses</HeaderLink>
+          <HeaderButton>Sign in</HeaderButton>
+          <HeaderButton isSecondary>Register</HeaderButton>
+        </Nav>
+      </HeaderInner>
+    </HeaderWrapper>
   );
 };
+
 Header.propTypes = {
   isSecondary: PropTypes.bool,
 };
---
+++ src/utils/styles/generalStyles.js
@@ -0,0 +1,42 @@
+import styled from "style-components";
+import { colors } from "./theme";
+
+export const Button = styled.Button`
+  display: inline-block;
+  width: 150px;
+  line-height: 48px;
+  font-size: 16px;
+  background-color: var(--color-secondary);
+  color: var(--color-primary);
+  border-radius: 30px;
+  font-weight: 500;
+  text-align: center;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: all 0.3s ease-out;
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+
+  ${(props) =>
+    props.isLanding &&
+    `
+    width: 220px;
+    `}
+
+  ${(props) =>
+    props.isSecondary &&
+    `
+    background-color: var(--color-primary);
+    color: var(--color-secondary);
+    `}
+
+    ${(props) =>
+    props.isOutline &&
+    `
+    width: 200px;
+    border: 1px solid var(--color-primary);
+    `}
+`;
